f8e8255ec815d1f2c392b730857d0bf6c7df0c1f
Pankaj Shah <shahpankaj981@gmail.com>
2022-12-26 18:52:21 -0600

    basic layour components like navbar and login done

--- resources/js/layouts/App.jsx
+++ resources/js/layouts/App.jsx
@@ -3,15 +3,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Navbar from "./Navbar";
 
 const App = () => {
     return(
         <BrowserRouter>
-            <h3>StreamStats</h3>
+            <Navbar />
             <Routes>
                 <Route exact path='/' element={ <Home /> } />
                 <Route path='/login' element={ <Login /> } />
-                
+
             </Routes>
         </BrowserRouter>
     );
--- resources/js/pages/Login.jsx
+++ resources/js/pages/Login.jsx
@@ -1,11 +1,61 @@
-import React from "react"
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Login = () => {
-    return(
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+
+const theme = createTheme();
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign in with Twitch
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-export default Login;
